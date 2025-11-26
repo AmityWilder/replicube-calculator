1a9e15cd2ca83eb7bfa931943db9d82c0b25b195
Amy Wilder <henrythepony@gmail.com>
2025-11-25 20:48:32 -0500

    Initial code

---
+++ calculate.py
@@ -0,0 +1,78 @@
+import ast
+
+
+def dfs(root: ast.AST) -> set[int] | set[tuple[int, int]] | set[tuple[int, int, int]]:
+    fields = {fieldname: value for fieldname, value in ast.iter_fields(root)}
+    if type(root) is ast.BinOp:
+        op = type(fields['op'])
+        left = dfs(fields['left'])
+        right = dfs(fields['right'])
+        # print(left, op.__name__, right)
+        if op is ast.Mult:
+            s = set()
+            for a in left:
+                if type(a) is tuple:
+                    x,y = a
+                    for z in right:
+                        s.add((x, y, z))
+                else:
+                    x=a
+                    for b in right:
+                        if type(b) is tuple:
+                            y,z = b
+                            s.add((x, y, z))
+                        else:
+                            y=b
+                            s.add((x, y))
+        elif op is ast.BitOr:
+            assert type(left) == type(right)
+            s = left.union(right)
+        elif op is ast.BitAnd:
+            assert type(left) == type(right)
+            s = left.intersection(right)
+        else:
+            raise Exception(op)
+        # print(s)
+        return s
+    elif type(root) is ast.Set:
+        s=set()
+        prev = None
+        is_ellipsis = False
+        for elt in fields['elts']:
+            elt_fields = {fieldname: value for fieldname, value in ast.iter_fields(elt)}
+            if type(elt) is ast.Constant:
+                x = elt_fields['value']
+                if x is ...:
+                    is_ellipsis = True
+                elif type(x) is int:
+                    # print(x)
+                    if is_ellipsis:
+                        is_ellipsis = False
+                        assert prev is not None
+                        for n in range(prev+1, x):
+                            print(n)
+                            s.add(n)
+                    s.add(x)
+                    prev = x
+                else:
+                    raise Exception(x)
+            else:
+                raise Exception(elt)
+        # print(s)
+        return s
+    else:
+        raise Exception(root)
+
+
+while True:
+    try:
+        s = input()
+        if s is None or s == "exit": break
+        tree = ast.parse(s)
+        tree = next(ast.iter_child_nodes(tree)) # skip Module
+        tree = next(ast.iter_child_nodes(tree)) # skip Expr
+        s = dfs(tree)
+        print(s)
+        print(sum((len(xyz) for xyz in s)))
+    except Exception as e:
+        print(e)
---
+++ readme.md
@@ -0,0 +1,63 @@
+# Replicube Calculator
+
+A calculator that provides the number of voxels present in a voxel set.
+
+## Syntax
+
+Empty set
+```py
+{}
+```
+
+1D set with coordinate $\langle 3 \rangle$
+```py
+{3}
+```
+
+1D set with coordinates $\{\langle 7 \rangle, \langle 9 \rangle\}$
+```py
+{7,9}
+```
+
+1D set with coordinates $\{\langle -2 \rangle, \dots, \langle 2 \rangle\}$ \
+i.e. $\{\langle -2 \rangle, \langle -1 \rangle,\langle 0 \rangle,\langle 1 \rangle, \langle 2 \rangle\}$
+```py
+{-2,...,2}
+```
+
+2D set with coordinate $\langle 2,7 \rangle$
+```py
+{2}*{7}
+```
+
+2D set with coordinates
+```math
+\begin{bmatrix}
+    \langle -2,-2 \rangle & \dots & \langle 2,-2 \rangle \\
+    \vdots & \ddots & \vdots \\
+    \langle -2,2 \rangle & \dots & \langle 2,2 \rangle \\
+\end{bmatrix}
+```
+```py
+{-2,...,2}*{-2,...,2}
+```
+
+3D set with coordinate $\langle 5,-6,1 \rangle$
+```py
+{5}*{-6}*{1}
+```
+
+3D set of a 5x5x5 cube
+```py
+{-2,...,2}*{-2,...,2}*{-2,...,2}
+```
+
+Union of sets $\{\langle 1 \rangle, \dots, \langle 3 \rangle\} \cup \{\langle -1 \rangle, \dots, \langle 2 \rangle\} = \{\langle -1 \rangle, \dots, \langle 3 \rangle\}$
+```py
+{1,...,3}|{-1,...,2}
+```
+
+Intersection of sets $\{\langle 1 \rangle, \dots, \langle 3 \rangle\} \cap \{\langle -1 \rangle, \dots, \langle 2 \rangle\} = \{\langle 1 \rangle, \dots, \langle 3 \rangle\}$
+```py
+{1,...,3}&{-1,...,2}
+```
